c1ef767ecf2c49ae404a936110e5cee512b15b35
PaulaP <piotrowska.paul@gmail.com>
2025-07-04 22:13:06 +0200

    polaczenie do service nbp i pobranie kursy USD i EUR

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+import asyncio
+from src.services.nbp import get_rate
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    """
+    Zwraca dzisiejszy kurs USD i EUR wg tabeli A NBP.
+    """
+    usd_rate, eur_rate = await asyncio.gather(
+        get_rate("USD"),
+        get_rate("EUR")
+    )
+    return {
+        "USD": usd_rate,
+        "EUR": eur_rate
+    }
